baf0e65730083f8a33372bb1ad77142f35cc5429
saiprasadkrishnamurthy <saiprasad.krishnamurthy@sita.aero>
2020-06-06 09:44:28 +0100

    First Push

---
+++ src/main/kotlin/com/sai/azure/demo/TrackingController.kt
@@ -0,0 +1,27 @@
+package com.sai.azure.demo
+
+import com.github.javafaker.Faker
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import java.time.Duration
+import java.util.stream.Stream
+
+@RestController
+class SimpleRoute {
+
+    data class Tracking(val reg: String, val location: String, val timestamp: Long)
+
+    val faker = Faker()
+
+    @GetMapping("/api/v1/track", produces = [MediaType.APPLICATION_STREAM_JSON_VALUE])
+    fun track(): Flux<Tracking> {
+        val interval = Flux.interval(Duration.ofMillis(500))
+        val events = Flux.fromStream { Stream.generate { sampleData() } }
+        return Flux.zip(events, interval).map { it.t1 }
+    }
+
+    private fun sampleData(): Tracking =
+            Tracking(faker.regexify("[A-Z1-9]{6}"), faker.address().fullAddress(), System.currentTimeMillis())
+}
